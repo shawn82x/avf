7621d8fdad0f1277bfcbacf1f24f418ae15046c1
Shawn Morgart <halcyon@fullsail.edu>
2012-05-17 01:01:45 -0400

    Work with PhoneGap Media Settings
    The goal is to provide a function for voice recording within the "Add
    Client Data" page of the app. As of this commit, it is not finding the
    'Media' element.

--- avf-ios/morgart_crud2/www/js/accel.js
+++ avf-ios/morgart_crud2/www/js/accel.js
@@ -7,26 +7,25 @@ function onBodyLoad(){
 // PhoneGap is ready
 
 function onDeviceReady(){
+
+    function getCurrentAcceleration() {
+        navigator.accelerometer.getCurrentAcceleration(onAccelerationSuccess, onError);
+    };
     
+    // onSuccess: Get current acceleration
+    
+    function onAccelerationSuccess(acceleration) {
+        
+        alert( 'Acceleration X: ' + acceleration.x + '<BR>' + 'Acceleration Y: ' + acceleration.y + '<BR>'
+              
+              + 'Acceleration Z: ' + acceleration.z + '<BR>');
+        
+    };
+    
+    // onError: Failed to get the acceleration
+    
+    function onError() {
+        
+        alert ("onError");
+    };
 }
-
-function getCurrentAcceleration() {
-    navigator.accelerometer.getCurrentAcceleration(onAccelerationSuccess, onError);
-}
-
-// onSuccess: Get current acceleration
-
-function onAccelerationSuccess(acceleration) {
-    
-    alert( 'Acceleration X: ' + acceleration.x + '<BR>' + 'Acceleration Y: ' + acceleration.y + '<BR>'
-          
-          + 'Acceleration Z: ' + acceleration.z + '<BR>');
-    
-}
-
-// onError: Failed to get the acceleration
-
-function onError() {
-    
-    alert ("onError");
-} 
--- avf-ios/morgart_crud2/www/js/recorder.js
+++ avf-ios/morgart_crud2/www/js/recorder.js
@@ -1,138 +1,51 @@
-// Recorder.js
+//Recorder.js
 
-// Creates a small Audio recorder
+// Wait for PhoneGap to load
+    //
+    document.addEventListener("deviceready", onDeviceReady, false);
+    
+     // PhoneGap is ready
+    //
+    function onDeviceReady() {
+        recordAudio();
+    }
 
-function record()
-    {        
-        if($('#record-button').attr('src') == 'images/stop.png')
-        {
-            stopRecord();
-        }
-        else
-        {
-            startRecord();
-        }
-    }
-    
-    function play()
-    {
-        if($('#play-button').attr('src') == 'images/play.png')
-        {
-            playRecord();
-        }
-        else
-        {
-            stopPlayback();
-        }
-    }
-    
-    function deleteAudio()
-    {
-        if($('#cd-button').attr('src') == 'help.png')
-        {
-            alert("Touch the record button to record a voice memo.");
-        }
-        else
-        {
-            if(confirm("Really delete audio recording?"))
-            {
-                deleteRecording();
+    // Record audio
+    // 
+    function recordAudio() {
+        var src = "myrecording.mp3";
+        var mediaRec = new Media(src, onSuccess, onError);
+
+        // Record audio
+        mediaRec.startRecord();
+
+        // Stop recording after 60 sec
+        var recTime = 0;
+        var recInterval = setInterval(function() {
+            recTime = recTime + 1;
+            setAudioPosition(recTime + " sec");
+            if (recTime >= 60) {
+                clearInterval(recInterval);
+                mediaRec.stopRecord();
             }
-        }
+        }, 1000);
     }
- 
-    function deleteRecording()
-    {
-        if(navigator.audio)
-        {
-            debug.log("resetting audio");
-            debug.log("removing old file at url " + navigator.audio.src);
-            var path = navigator.audio.src.substr('documents://'.length);
-            debug.log("Deleting audio file at path: " + path + " from url: "+navigator.audio.src);
-            navigator.fileMgr.deleteFile(path,file_success,file_failed);
-            navigator.audio = null;
-            $('#cd-button').attr('src','help.png');
-        }
-        else
-        {
-            alert("Touch the record button to record a voice memo.");
-        }
+
+    // onSuccess Callback
+    //
+    function onSuccess() {
+        console.log("recordAudio():Audio Success");
     }
- 
-function startRecord()
-{
-        debug.log("start record...");
-        if(navigator.audio && confirm("Delete existing recording?"))
-        {
-            deleteRecording();
-        }
- 
-        debug.log("Initializing audio");
-        navigator.audio = new Media(null,recording_success,recording_failure);
-        $('#cd-button').attr('src','cd.png');
-        debug.log("Starting recording...");
-        navigator.audio.startAudioRecord();
-        navigator.notification.activityStart();
-        $('#record-button').attr('src','stop.png');
-navigator.audio.startAudioRecord();
-}
-function stopRecord()
-{
-        debug.log("Stopping recording...");
-        $('#record-button').attr('src','record.png');
-        navigator.notification.activityStop();
-        $('#cd-button').attr('src','cd_remove.png');
-navigator.audio.stopAudioRecord();
-}
- 
-function playRecord()
-{
-        debug.log("Playing recording...");
-        if(navigator.audio)
-        {
-            $('#play-button').attr('src','stop.png');
-            navigator.notification.activityStart();
-            navigator.audio.play();
-        }
-        else
-        {
-            alert("No audio. Touch the record button to record some.");
-        }
-}
-    
-    function stopPlayback()
-    {
-        debug.log("Stopping playback...");
-        $('#play-button').attr('src','play.png');
-        navigator.notification.activityStop();
-navigator.audio.stop();
+
+    // onError Callback 
+    //
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' + 
+              'message: ' + error.message + '\n');
     }
- 
-    function file_success()
-    {
-        debug.log("file deleted");
-        $('#cd-button').attr('src','help.png');
+
+    // Set audio position
+    // 
+    function setAudioPosition(position) {
+        document.getElementById('audio_position').innerHTML = position;
     }
-    
-    function file_failed()
-    {
-        debug.log("file not deleted");
-    }
-    
-    function recording_success(url)
-    {
-        debug.log("Recording success callback");
-        navigator.notification.activityStop();  
-        $('#record-button').attr('src','record.png');
-        $('#play-button').attr('src','play.png');
-        $('#cd-button').attr('src','cd_remove.png');
-    }
-    
-    function recording_failure(error)
-    {
-        navigator.notification.activityStop();  
-        alert("Recording failed: " + error);
-        debug.log("Recording failed: " + error);
-    }
- 
- 
